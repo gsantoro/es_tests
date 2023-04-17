b907f80efb0f36f5ad8168ed19ed13f4dd9b3db2
gsantoro <giuseppe.santoro@elastic.co>
2023-04-17 15:33:04 +0100

    adding go needed to install elastic-package

--- devenv.nix
+++ devenv.nix
@@ -4,12 +4,12 @@
   # https://devenv.sh/packages/
   packages = [
      pkgs.git 
+     pkgs.go 
      pkgs.go-task
      pkgs.direnv
   ];
 
   scripts.devenv-install.exec = "nix profile install --accept-flake-config github:cachix/devenv/latest";
-  scripts.t.exec = "task";
 
   scripts.direnv-allow.exec = "direnv allow";
   scripts.direnv-reload.exec = "direnv reload";
